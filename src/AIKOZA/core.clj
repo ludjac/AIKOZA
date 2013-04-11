bba9f86500b4bec2c8d6ae7f6016f4f011508e3b
ludjac <ludjac@gmail.com>
2013-04-11 17:32:56 +0300

    Optimizing with list of population
    Minimizing fitness evals by introducing a list of individuals and their fitness. 

--- src/AIKOZA/core.clj
+++ src/AIKOZA/core.clj
@@ -1,5 +1,12 @@
-(ns AIKOZA.core)
+(ns AIKOZA.core (:use [taoensso.timbre.profiling :as profiling :only (p profile)]))
 (require ['clojure.zip :as 'z])
+;;(require ['clojure.contrib.profile :as 'pro])
+
+
+; lista med populationen, uppdateras vid varje ny generation. ([individ1 fitness][individ2 fitness][osv...])
+(def population (atom ()))
+
+
 
 (defn in? 
   "returns true if ele is in seq"
@@ -56,9 +63,9 @@
 
 ; Returnerar nil om passande indiviad saknas,
 ; returnerar bästa individen om det finns fler
-(defn finsihedpop [popul limit]
-  (let [topind (apply min-key #(fitness (tfun %) goalf) popul)]
-    (if (> limit (fitness (tfun topind) goalf))
+(defn finsihedpop [limit]
+  (let [topind (first (sort-by #(second %) @population))]
+    (if (> limit (second topind))
       topind
       nil
       )
@@ -67,7 +74,7 @@
 
 
 ;startpopulationen i kozas artikel
-(def population '((- (+ x 1) 0)
+(def population-koza '((- (+ x 1) 0)
                  (+ 1 (* x x))
                  (+ 2 0)
                  (* x (- -1 -2))))
@@ -95,12 +102,13 @@
 ;fitnessfunktion, tar en individ och en målfunktion om returnerar snittet
 ;av kvadratiska avståndet mellan funktionerna (-1 till 1 med 0.1 som steg)
 (defn fitness [ind goal]
+  (p :fitness
   (let [r (range -1 1 0.1)]
     (/ (reduce + 
          (map #(* % %) 
            (map - 
              (map goal r) (map ind r))))
-       (count r))))
+       (count r)))))
 
 ;returnerar true ibland (tex 90% av fallen om x är 0.9)
 (defn ptrue [x]
@@ -123,6 +131,7 @@
                      (if (and (not (z/branch? loc)) (not (leftm? loc)))
                        [c]))
              (inc c)))))
+
 ;returnerar hur många gånger man behöver "nexta" ett träd för at hita subträden
 (defn zipsub2 [expr]
   (next (loop [loc (z/seq-zip (seq expr)) r '() c 0]
@@ -156,28 +165,73 @@
 
 ;plockar ut den bästa (enligt målfunktionen g) av n stycken slumpmässiga individer ur populationen pop
 (defn tournsel [pop n g]
-  (first (min-key #(fitness (tfun %) g)
-                (repeatedly n #(rand-nth pop)))))
-
-
-
-(defn nextgen [pop g n]
-  (let [spop (sort-by #(fitness (tfun %) g) pop) r (take n spop) k (- (count pop) n)]
-    (concat r (repeatedly k #(let [i (tournsel pop 10 g) nn (rand-nth (zipsub i))]
-                     (zipmerge i nn (subtrees (tournsel pop 10 g))))))))
+  (p :tournsel (first (min-key #(fitness (tfun %) g)
+                (repeatedly n #(rand-nth pop))))))
     
+(defn nextgen 
+  [g n]
+ (p :nextgen 
+    (let 
+      [pop (map #(first %) @population)
+       spop (sort-by #(second %) @population) 
+       r (take n spop) 
+       k (- (count @population) n)
+       new-ind (repeatedly k 
+                        #(let 
+                           [i (tournsel pop 10 g) 
+                            nn (rand-nth (zipsub i))]
+                     (zipmerge i nn (subtrees (tournsel pop 10 g)))))
+       ]
+    (reset! population (concat r (zipmap new-ind (map #(fitness (tfun %) g) new-ind)))))
+    )
+ )
 
-; generate population of size size and depth 2 or 3. 
-(defn gen_population 
-  [size]
-  (take size (repeatedly #(gen_rnd_expr (rand-nth '(2 3)))))
+
+;(defn nextgen [pop g n]
+; (p :nextgen (let [spop (sort-by #(fitness (tfun %) g) pop) r (take n spop) k (- (count pop) n)]
+;    (concat r (repeatedly k #(let [i (tournsel pop 10 g) nn (rand-nth (zipsub i))]
+;                     (zipmerge i nn (subtrees (tournsel pop 10 g)))))))))
+    
+; generate population to atom list
+
+(defn gen-pop
+  [size g]
+  (let 
+    [pop (take size (repeatedly #(gen_rnd_expr (rand-nth '(2 3)))))]
+    (reset! population (zipmap pop (map #(fitness (tfun %) g) pop))))
   )
 
-(defn bestind [pop]
-  (apply min (map #(fitness (tfun %) goalf) pop)))
+; generate population of size size and depth 2 or 3. 
+;(defn gen_population 
+;  [size]
+;  (take size (repeatedly #(gen_rnd_expr (rand-nth '(2 3)))))
+;  )
+
+(defn bestind []
+  (p :bestind
+  (first (sort-by #(second %) @population))))
 
 (defn bestf [pop]
-  (apply min-key #(fitness (tfun %) goalf) pop))
+  (p :bestf
+  (apply min-key #(second %) pop)))
+
+(defn -main [pop-size goal]
+  (gen-pop pop-size goal)
+  (loop [c 0]
+    (if-let [r (or (finsihedpop 0.01) (> c 1000))]
+      (do (println c) (println (bestind)))
+      (do (println c) (println (bestind)) (nextgen goalf (int (* (count @population) 0.1))) (recur (inc c) )))))
+
+;;(add-box-plot gplot (map #(fitness (tfun %) goalf) cpop))
+;
+;(defn -main [spop]
+; 
+;  (loop [cpop spop c 0]
+;    (if-let [r (or (finsihedpop 0.1) (> c 10))]
+;      (do (println c) (bestf cpop))
+;      (do (println c) (println (bestind cpop)) (recur (nextgen goalf 3) (inc c))))))
+
+;;;;;;--------------- Plottar och övrigt skit --------------------;
 
 (use '(incanter core stats charts))
 ; Histogrammar fitness från en populaiton. 
@@ -199,12 +253,4 @@
   )
 
 (def gplot (box-plot []))
-
-(defn -main [spop]
-  (view gplot)
-  (loop [cpop spop c 0]
-    (if-let [r (or (finsihedpop cpop 0.01) (> c 50))]
-      (do (println c) (plot (tfun (bestf cpop)) goalf) (bestf cpop))
-      (do (println c) (println (bestind cpop)) (add-box-plot gplot (map #(fitness (tfun %) goalf) cpop)) (recur (nextgen cpop goalf 3) (inc c))))))
-
 
