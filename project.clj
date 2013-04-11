5e7503f9f5f9a1ec37832071ea7c91a30582a9fd
ludjac <ludjac@gmail.com>
2013-04-11 17:31:12 +0300

    Dependencies for profiling and plotting
    Incanter & taoensso/timbre

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"][incanter "1.2.3-SNAPSHOT" :exclusions [incanter/incanter-mongodb]][com.taoensso/timbre "1.5.2"]])
